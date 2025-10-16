79deb134187280e0ab4d7e882701ff923d38160b
Nataliia-Gennadievna <136744472+Nataliia-Gennadievna@users.noreply.github.com>
2025-10-16 19:14:57 +0300

    Initial commit

---
+++ src/main/java/com/example/Catering/entity/User.java
@@ -0,0 +1,40 @@
+package com.example.Catering.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password; // будет хэшироваться позже
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        CLIENT,
+        STAFF,
+        ADMIN
+    }
+}
